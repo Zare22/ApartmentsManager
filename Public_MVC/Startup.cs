1d9700f617714fe30f28725adb0bb27a48d6f8c6
Leo <81168288+Zare22@users.noreply.github.com>
2023-01-25 21:46:22 +0100

    MVC_Start

---
+++ Public_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Public_MVC.Startup))]
+namespace Public_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
